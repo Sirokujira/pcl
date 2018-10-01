f1cc1fe06d0da1e756ee7b3612506e9319e60d59
sirokujira <toru753@gmail.com>
2018-10-01 17:30:01 +0900

    update build command

--- build.sh
+++ build.sh
@@ -20,11 +20,32 @@ cmake \
   -DWITH_QHULL=OFF \
   -DWITH_QT=OFF \
   -DWITH_VTK=OFF \
-  -DBUILD_global_tests=OFF \
   -DBUILD_examples=OFF \
   -DBUILD_tools=ON \
   -DBUILD_apps=OFF \
+  -DBUILD_global_tests=ON \
+  -DBUILD_tests_2d=OFF \
+  -DBUILD_tests_common=OFF \
+  -DBUILD_tests_features=OFF \
+  -DBUILD_tests_filters=OFF \
+  -DBUILD_tests_geometry=OFF \
+  -DBUILD_tests_io=OFF \
+  -DBUILD_tests_keypoints=OFF \
+  -DBUILD_tests_octree=OFF \
+  -DBUILD_tests_outofcore=OFF \
+  -DBUILD_tests_people=OFF \
+  -DBUILD_tests_recognition=OFF \
+  -DBUILD_tests_registration=OFF \
+  -DBUILD_tests_sample_consensus=OFF \
+  -DBUILD_tests_segmentation=OFF \
+  -DBUILD_tests_surface=OFF \
+  -DBUILD_tests_visualization=OFF \
+  -DBUILD_tests_kdtree=ON \
+  -DBUILD_tests_search=ON \
   ..
 
-make -j 4
-make install
+# make -j 4
+# make install
+cmake --build . --config Release --target install
+ctest -C Release -V
+
